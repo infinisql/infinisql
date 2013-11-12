1cbe49b66c188c258c2c677f1a00379c79aecf8b
mtravis <mtravis@mtraviss-MacBook.local>
2013-11-12 01:22:59 -0800

    Doc changes after proof-reading. Ready for publicity.

--- docs/pubs/makexhtml.sh
+++ docs/pubs/makexhtml.sh
@@ -2,9 +2,9 @@
 
 # should have something such that for each file (unless chunked) it does
 # aS FOLLOWS:
-# xsltproc -o <infinisql.github.io/pubs/file.html /Users/mtravis/DocBook/docbook-xsl-ns-1.78.1/xhtml/docbook.xsl infile.xml
+# xsltproc -o <infinisql.github.io/file.html /Users/mtravis/DocBook/docbook-xsl-ns-1.78.1/xhtml/docbook.xsl infile.xml
 
 filename=$(basename $1)
 filename="${filename%.*}"
-xsltproc --xinclude -o ~/projects/infinisql.github.io/pubs/$filename.html /Users/mtravis/DocBook/docbook-xsl-ns-1.78.1/xhtml/docbook.xsl $1
+xsltproc --xinclude -o ./html/$filename.html /Users/mtravis/DocBook/docbook-xsl-ns-1.78.1/xhtml/docbook.xsl $1
 
