7727d1039add66ab3adaaaaa6f6e6c713f634e91
Christopher Nelson <nadiasvertex@gmail.com>
2013-12-11 13:14:31 -0500

    Moved unit test to actual unit test, fixed resource leaks, verified announce functionality.

--- manager/infinisqlmgr/management/__init__.py
+++ manager/infinisqlmgr/management/__init__.py
@@ -6,6 +6,7 @@ import select
 import signal
 import socket
 import struct
+import time
 
 import msgpack
 import zmq
@@ -18,8 +19,11 @@ class Controller(object):
 
         self.cmd_socket = self.ctx.socket(zmq.PUB)
         self.sub_sockets = []
+        self.nodes = set()
 
         self.presence_recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.last_presence_announcement = 0
+        self.presence_announcement_period = 1
 
         self.cluster_name = cluster_name
         self.mcast_group = mcast_group
@@ -54,8 +58,10 @@ class Controller(object):
         try:
              res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
         except:
-            sock.close()
             return None
+        finally:
+            sock.close()
+            
         ip = struct.unpack('16sH2x4s8x', res)[2]
         return socket.inet_ntoa(ip)
 
@@ -78,13 +84,25 @@ class Controller(object):
                           self.cluster_name, cluster_name)
             return
 
+        # Drop the announcement if we are receiving our own broadcast
+        if remote_ip == self._get_ip() and \
+            remote_port == self.cmd_port:
+            logging.debug("dropping add_node because I have caught my own announcement")
+            return
+
+        node_id = (remote_ip, remote_port)
+        if node_id in self.nodes:
+            logging.debug("dropping add_node because we already know this node")
+
         # Subscribe to the remote management node.
         remote_pub = "tcp://%s:%d" % (remote_ip, remote_port)
         sub_sock = self.ctx.socket(zmq.SUB)
         sub_sock.connect(remote_pub)
+        self.sub_sockets.append(sub_sock)
 
         # Register the new subscription socket.
         self.poller.register(sub_sock, flags=zmq.POLLIN)
+        self.nodes.add(node_id)
 
     def _process_publication(self, sock):
         """
@@ -97,17 +115,40 @@ class Controller(object):
 
     def _process_presence(self, sock):
         data = sock.recv(4096)
-        remote_ip, cluster_name, remote_port = msgpack.unpackb(data)
+        cluster_name, remote_ip, remote_port = msgpack.unpackb(data)
         self.add_node(cluster_name.decode("ascii"), remote_ip.decode("ascii"), remote_port)
 
-    def announce_presence(self):
+    def announce_presence(self, force=False):
         """
         Send a presence announcement to the local network segment.
+        :param force: If set to true the announcement is forced regardless of the last time an announcement was made.
         """
+        now = time.time()
+        if  (now - self.last_presence_announcement < self.presence_announcement_period) and \
+            force==False:
+            return
+
+        self.last_presence_announcement = now
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 8)
-        sock.sendto(msgpack.packb([self._get_ip(), self.cluster_name, self.cmd_port]), (self.mcast_group, self.mcast_port))
+        sock.sendto(msgpack.packb([self.cluster_name, self._get_ip(), self.cmd_port]), (self.mcast_group, self.mcast_port))
         sock.close()
+
+    def get_nodes(self):
+        """
+        Provides a set of node ids.
+        :return: A set of node ids.
+        """
+        return self.nodes
+
+    def is_node_known(self, node_id):
+        """
+        Indicates if the node_id is known to this manager.
+
+        :param node_id: The node id to look for.
+        :return: True if it's known, False otherwise.
+        """
+        return node_id in self.nodes
 
     def _stop_signal_handler(self, signum, frame):
         """
@@ -158,3 +199,14 @@ class Controller(object):
         logging.info("Stopped management process.")
         signal.signal(signal.SIGTERM, signal.SIG_DFL)
         return 0
+
+    def shutdown(self):
+        self.stop()
+        self.presence_poller.unregister(self.presence_recv_socket)
+        for sock in self.sub_sockets:
+            self.poller.unregister(sock)
+
+        self.presence_recv_socket.close()
+        self.cmd_socket.close()
+        for sock in self.sub_sockets:
+            sock.close()
--- manager/tests/test_announce_presence.py
+++ manager/tests/test_announce_presence.py
@@ -1,16 +1,41 @@
 __author__ = 'Christopher Nelson'
 
-import asyncore
-import logging
 import os
 import sys
+import unittest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 
 from infinisqlmgr import management
 
-logging.basicConfig(level=logging.DEBUG)
+class TestAnnouncePresence(unittest.TestCase):
+    def setUp(self):
+        self.c1 = management.Controller("default_cluster", cmd_port=11000)
+        self.c2 = management.Controller("default_cluster", cmd_port=12000)
 
-c = management.Controller("default_cluster", cmd_port=11000)
-c.announce_presence()
-c.process()
+    def tearDown(self):
+        self.c1.shutdown()
+        self.c2.shutdown()
+
+    def test_announce_ignores_self(self):
+        self.c1.announce_presence()
+        self.c1.process()
+        self.c2.process()
+
+        self.assertEqual(0, len(self.c1.get_nodes()))
+        self.assertEqual(1, len(self.c2.get_nodes()))
+
+    def test_mutual_announce(self):
+        self.c1.announce_presence()
+        self.c2.announce_presence()
+
+        for i in range(0, 2):
+            self.c1.process()
+            self.c2.process()
+
+        self.assertEqual(1, len(self.c1.get_nodes()))
+        self.assertEqual(1, len(self.c2.get_nodes()))
+
+
+if __name__ == "__main__":
+    unittest.main()
