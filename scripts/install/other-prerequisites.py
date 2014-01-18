c5a6e4b09133c616e022a192addb77517da42e0e
Christopher Nelson <nadiasvertex@gmail.com>
2014-01-18 14:53:44 -0500

    Added lmdb to prerequisite fetch.

--- scripts/install/other-prerequisites.py
+++ scripts/install/other-prerequisites.py
@@ -15,15 +15,21 @@ def copy_lib(from_path):
       os.makedirs(lib_folder)
    lib_name = os.path.split(from_path)[1]
    target = os.path.join(lib_folder, lib_name)
-   shutil.copyfile(from_path, target)
    print("Installing library: %s -> %s" %(from_path, target))
+   shutil.copyfile(from_path, target)
    
 def copy_inc(from_path, dst_name):
     if not os.path.exists(inc_folder):
       os.makedirs(inc_folder)
     to_path = os.path.join(inc_folder, dst_name)
     print("Installing headers: %s -> %s"%(from_path, to_path))
-    shutil.copytree(from_path, to_path)
+    
+    if os.path.isdir(from_path):
+        if os.path.exists(to_path):
+            shutil.rmtree(to_path, ignore_errors=True)
+        shutil.copytree(from_path, to_path)
+    else:
+        shutil.copyfile(from_path, to_path)
    
 def find_folder_with_file(start_path, find_filename):
    for root, dirs, files in os.walk(start_path):
@@ -43,9 +49,20 @@ def tbb(archive_path):
    copy_lib(os.path.join(tbb_lib_path, "libtbb.so.2"))
    copy_inc(tbb_inc_path, "tbb")
    
+def lmdb(archive_path):
+    archive_folder, archive_name = os.path.split(archive_path)
+    os.system('tar -C "%s" -xzf "%s"' % (archive_folder, archive_path))
+    os.chdir(os.path.join(archive_folder, "mdb-mdb", "libraries", "liblmdb"))
+    os.system("make")
+    copy_lib(os.path.join(os.getcwd(), "liblmdb.so"))
+    copy_lib(os.path.join(os.getcwd(), "liblmdb.a"))
+    copy_inc(os.path.join(os.getcwd(), "lmdb.h"), "lmdb.h")
+    
 #===============================================================================
 packages = [("https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb42_20131118oss_src.tgz",
-             tbb)]
+             tbb),
+            ("https://gitorious.org/mdb/mdb/archive/aa3463ec7c5e979420b13c8f37caa377ed2c1cf1.tar.gz",
+             lmdb)]
 
 #===============================================================================
 tmp_folder = os.environ.get("TEMP", "/tmp") 
