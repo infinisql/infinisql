37a49852bd087458c29fce7896f5c080734ea64a
Christopher Nelson <nadiasvertex@gmail.com>
2014-01-19 10:28:44 -0500

    Fix an improper delete bug.

--- src/engine/Serdes.cc
+++ src/engine/Serdes.cc
@@ -27,7 +27,7 @@
 
 #include "Serdes.h"
 
-Serdes::Serdes() : isreadonly(false), pos(0), val({0, nullptr})
+Serdes::Serdes() : isreadonly (false), pos (0), val ({0, nullptr})
 {
     
 }
@@ -45,7 +45,7 @@ Serdes::Serdes(size_t mv_sizearg) : Serdes()
     }
 }
 
-Serdes::Serdes(MDB_val &valarg) : isreadonly(true), pos(0), val(valarg)
+Serdes::Serdes(MDB_val &valarg) : isreadonly (true), pos (0), val (valarg)
 {
     
 }
