7701671f62076188039a0b0d04e1a32182fbec58
Christopher Nelson <nadiasvertex@gmail.com>
2014-01-19 10:26:39 -0500

    Added symbolized address sanitizer info.

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
-scons -Q asan=1 verbose=1 && mv -f tests/test . && ASAN_OPTIONS="detect_leaks=1" LD_LIBRARY_PATH=deps/lib ./test
+
+scons -Q asan=1 verbose=1 && mv -f tests/test . && ASAN_OPTIONS="detect_leaks=1" ASAN_SYMBOLIZER_PATH=`which llvm-symbolizer-3.4` LD_LIBRARY_PATH=deps/lib ./test
 
